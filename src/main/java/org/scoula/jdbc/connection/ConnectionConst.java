81ae8a3d3379ad8e63884ff6aac6d74cbc2447a0
bagjuna <juna.park124@gmail.com>
2025-06-04 23:57:52 +0900

    aop 완성

--- src/main/java/org/scoula/jdbc/connection/ConnectionConst.java
+++ src/main/java/org/scoula/jdbc/connection/ConnectionConst.java
@@ -5,5 +5,8 @@ public abstract class ConnectionConst {
     public static final String USERNAME = "root";
     public static final String PASSWORD = "juna1234";
 
+//    public static final String URL = "jdbc:postgresql://localhost:5432/test1";
+//    public static final String USERNAME = "postgres";
+//    public static final String PASSWORD = "ming";
 
 }
